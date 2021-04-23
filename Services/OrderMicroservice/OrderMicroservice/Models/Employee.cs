82ebb126431ff3beb9694000209d81ffa42efb1b
ratheesh <ratheesh.ps@gmail.com>
2021-04-23 17:14:46 +0530

    initial commit

---
+++ Services/OrderMicroservice/OrderMicroservice/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderMicroservice.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
